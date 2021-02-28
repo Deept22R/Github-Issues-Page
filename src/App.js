61901e5bd7b7050e98401610229deceb3bf1f035
Deepti <deepti220398@gmail.com>
2021-02-28 22:46:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+// import { Badge, InputGroup, FormControl, Button } from "react-bootstrap";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Options from "./Components/Options";
+// import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import IssuesPage from "./Components/IssuesPage";
+import Fork from "./Components/Fork";
+import Filter from "./Components/Filter";
+import IssueManager from './Components/IssueManager';
 function App() {
+  const [issue, setIssue] = useState("");
+  const [issueApi, setIssueApi] = useState();
+  const fetchIssues = () =>{
+      fetch(
+        "https://api.github.com/repos/facebook/create-react-app/issues"
+      )
+      .then((res)=>res.json())
+        .then((result)=>{
+          // console.log(result);
+          setIssue("not empty")
+            setIssueApi({result});
+            console.log(setIssueApi);
+        })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <br></br>
+      <Fork />
+      <Options />
+      <Filter />
+      <IssuesPage />
+      {issue===""?fetchIssues():null}
+      <IssueManager
+       issue = {issue}
+       setIssue = {setIssue}
+       fetchIssues = {fetchIssues}
+       issueApi = {issueApi}
+      />
+    </>
   );
 }
 
